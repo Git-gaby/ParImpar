2ae92b80c3f5ca65c2f7c8f9121a2656d15b7795
Gabriel <gabriel0322024@gmail.com>
2025-08-11 14:59:37 -0500

    ejercicio Parimpar

--- 02-ParImpar.py
+++ 02-ParImpar.py
@@ -1,1 +1,14 @@
-print("hola")
+print("Hola, bienvenido a mi tercer ejercio en Python")
+print()
+print("si un numero es par o impar")
+print()
+n1 = int(input("ingrese el numero que desea comprobar"))
+
+if n1 % 2 ==0  :
+    print(f"el numero, {n1} es par")
+elif n1 < 0 : 
+    print("debe ingresar un numero entro positivo")
+else:
+    print(f"el numero, {n1} es impar")
+print()
+print("Gracias por participar")
